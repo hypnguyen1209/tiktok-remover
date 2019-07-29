0d043fad41b6ce127355fa6135cf2af71929d3a4
Nguyen Van Hiep <hiep12092001@gmail.com>
2019-07-29 08:09:53 +0700

    Update chatbottiktok2.php

--- chatbottiktok2.php
+++ chatbottiktok2.php
@@ -1,8 +1,8 @@
 <?php
 $url = $_GET['url'];
-  $ch = curl_init('https://tiktok-remover.herukuapp.com/tiktok.php?url='.$url);
+  $ch = curl_init('https://tiktok-remove.herukuapp.com/tiktok.php?url='.$url);
     curl_setopt_array($ch, array(
-        CURLOPT_URL => 'https://tiktok-remover.herukuapp.com/tiktok.php?url='.$url,
+        CURLOPT_URL => 'https://tiktok-remove.herukuapp.com/tiktok.php?url='.$url,
         CURLOPT_SSL_VERIFYPEER => false,
         CURLOPT_SSL_VERIFYHOST => false,
         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.0 Safari/537.36',
